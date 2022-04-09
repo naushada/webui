3702f0e16a32bcb28f658b5136db8b71c867bcf0
naushada <naushad.dln@gmail.com>
2022-04-09 16:42:59 +0530

    Updated

--- src/app/shared/rest-api.service.ts
+++ src/app/shared/rest-api.service.ts
@@ -14,10 +14,10 @@ export class RestApiService {
     })
   } 
 
-  private apiURL:string = 'https://apigw-l7rsmphepa-uc.a.run.app';
+  //private apiURL:string = 'https://apigw-l7rsmphepa-uc.a.run.app';
   
   //private apiURL:string = 'http://localhost:8080';
-  //private apiURL:string = 'https://balaagh.herokuapp.com';
+  private apiURL:string = 'https://balaagh.herokuapp.com';
   //private apiURL:string = 'https://gwx.herokuapp.com';
   //private apiURL:string = 'https://xapp-cpkpi52p2q-uc.a.run.app';
   //private apiURL: string = 'https://xpmile.herokuapp.com'
