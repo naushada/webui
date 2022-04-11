f0e90c266819bb2c91f76c3f6c47b994038d1143
naushada <naushad.dln@gmail.com>
2022-04-11 23:40:41 +0530

    Updated

--- src/app/shipping/new-shipment/new-shipment.component.ts
+++ src/app/shipping/new-shipment/new-shipment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { RestApiService } from 'src/app/shared/rest-api.service';
 import { MatAccordion }  from '@angular/material/expansion';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-new-shipment',
@@ -13,9 +14,56 @@ export class NewShipmentComponent implements OnInit {
   newShipmentForm!: FormGroup;
   constructor(private fb: FormBuilder, private rest:RestApiService) { 
     this.newShipmentForm = this.fb.group({
+      activity : this.fb.array([{date: formatDate(new Date(), 'dd/MM/yyyy', 'en'),
+        event: 'Document Created',
+        time:new Date().getHours() + ':' + new Date().getMinutes(), 
+        notes: 'Document Created',
+        driver:'',
+        updatedBy:'',
+        eventLocation: 'Riyadh'
+      }]),
       shipmentNo: '',
       autogenerate: false,
-      altRefNo:''
+      altRefNo:'',
+      
+      //Sender Information
+      referenceNo:'',
+      accountCode:'',
+      companyName:'',
+      name:'',
+      country:'',
+      address:'',
+      city:'',
+      state:'',
+      postalCode:'',
+      contact:'',
+      phone:'',
+      email:'',
+      recvCountryTadId:'',
+      //Shipment Information
+      sku:'',
+      service:'',
+      noOfItems:'',
+      description:'',
+      goodsValue:'',
+      customValue:'',
+      weight:'',
+      weightUnit:'',
+      cubicWeight:'',
+      codAmount:'',
+      vat:'',
+      currency:'',
+      //Receiver Information
+      receiverName:'',
+      receiverCountry:'',
+      receiverAddress:'',
+      receiverCity:'',
+      receiverState:'',
+      receiverPostalCode:'',
+      receiverContact:'',
+      receiverPhone:'',
+      receiverEmail:''
+
     });
   }
 
