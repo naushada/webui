195377635abb76f10a77e686c090cd86ea94e3f6
naushada <naushad.dln@gmail.com>
2022-08-07 23:17:09 +0530

    API Integration

--- src/app/shipping/third-part-integ/third-part-integ.component.ts
+++ src/app/shipping/third-part-integ/third-part-integ.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Account } from 'src/app/shared/message-struct';
+import { PubsubService } from 'src/app/shared/pubsub.service';
+import { RestApiService } from 'src/app/shared/rest-api.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-third-part-integ',
@@ -9,12 +13,15 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ThirdPartIntegComponent implements OnInit {
 
   thirdPartyApiForm!: FormGroup;
+  acctInfo!: Account;
+  subsink = new SubSink();
+  m_vendor: string = "";
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private rest:RestApiService, private pubsub:PubsubService) {
     this.thirdPartyApiForm = this.fb.group({
       dhlExpress: false,
       aramex: false,
-      dtdc: false,
+      ajoul: false,
       sipsy: false,
       blueDartExpress: false,
       xlsUpload: ''
@@ -22,6 +29,8 @@ export class ThirdPartIntegComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    //this.subsink.sink = this.pubsub.onAccount.subscribe((acct: Account) => {this.acctInfo = acct;});
+    this.subsink.add(this.pubsub.onAccount.subscribe((acct: Account) => {this.acctInfo = acct;}));
   }
 
   onXlsSelect(e: any) {
@@ -30,10 +39,27 @@ export class ThirdPartIntegComponent implements OnInit {
 
   onCreateThirdPartyShipment()
   {
+    let uri: string = "";
+    if(this.m_vendor == 'ajoul') {
+      uri = "/api/v1/ajoul/shipment/create";
+      alert("the vendor is " + this.m_vendor);
+    } else {
+      return;
+    }
 
+    let awbList: any = {v1: "ajoul"};
+
+    this.rest.create3rdPartyShipment(awbList, uri, this.acctInfo.accountCode).subscribe(data=> {alert(data);},
+      (error:any) => {alert("Failed for Ajoul");},
+      () => {alert("Completed For Ajoul");});
+  }
+
+  setVendorName(vendor:string) {
+    this.m_vendor = vendor;
   }
 
   onCheckboxSelect(vendor:string) {
-    alert("The check box selected is " + vendor);
+    //alert("The check box selected is " + vendor);
+    this.m_vendor = vendor;
   }
 }
